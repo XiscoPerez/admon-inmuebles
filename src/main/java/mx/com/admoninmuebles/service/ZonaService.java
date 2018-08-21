c2b22d1fa2ca0dd3b634639e9c4450fe7d82e83d
FRANCISCO PEREZ LOPEZ <xisco.perez18@gmail.com>
2018-08-20 22:01:38 -0500

    Revert "Fix de auditoria, ejemplo en zona."

--- src/main/java/mx/com/admoninmuebles/service/ZonaService.java
+++ src/main/java/mx/com/admoninmuebles/service/ZonaService.java
@@ -1,12 +1,8 @@
 package mx.com.admoninmuebles.service;
 
-import java.util.Optional;
-
 import mx.com.admoninmuebles.dto.ZonaDto;
 import mx.com.admoninmuebles.persistence.model.Zona;
 
 public interface ZonaService {
     Zona save(ZonaDto zonaDto);
-
-    Optional<ZonaDto> findById(Long id);
 }
